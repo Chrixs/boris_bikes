28a936c1ecc4d151d17a64d2ee6720c88e5bfc1c
Chris <chrixs99@hotmail.com>
2016-11-09 19:07:45 +0000

    challenge 15 completed, refactored methods and made some private

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,17 +2,29 @@ require_relative "./bike"
 
 class DockingStation
   attr_reader :docked_bikes
+
   def initialize
     @docked_bikes = []
   end
+
   def release_bike
-    raise "Docking station is empty" if @docked_bikes.empty?
+    raise "Docking station is empty" if empty?
     @docked_bikes.pop
   end
 
   def dock(bike)
-    raise "Docking station is full" if @docked_bikes.count == 20
+    raise "Docking station is full" if full?
     @docked_bikes << bike
     @docked_bikes.any?
   end
+  
+  private
+
+    def empty?
+      @docked_bikes.empty?
+    end
+
+    def full?
+      @docked_bikes.count == 20
+    end
 end
